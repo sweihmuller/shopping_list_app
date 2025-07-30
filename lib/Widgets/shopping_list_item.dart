6d7d6c8f581f2fb9c1e726e2ba3f733f1737bcb1
Santi <sweihmuller01@gmail.com>
2025-07-30 02:40:31 -0300

    Dismiss item

--- lib/Widgets/shopping_list_item.dart
+++ lib/Widgets/shopping_list_item.dart
@@ -5,26 +5,37 @@ class ShoppingListItem extends StatefulWidget {
   const ShoppingListItem({super.key, required this.groceryItem});
 
   final List<GroceryItem> groceryItem;
-
   @override
   State<ShoppingListItem> createState() => _ShoppingListItemState();
 }
 
 class _ShoppingListItemState extends State<ShoppingListItem> {
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      widget.groceryItem.remove(item);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return widget.groceryItem.isEmpty
         ? const Center(child: Text('No items added yet.'))
         : ListView.builder(
             itemCount: widget.groceryItem.length,
-            itemBuilder: (ctx, index) => ListTile(
-              title: Text(widget.groceryItem[index].name),
-              leading: Container(
-                width: 24,
-                height: 24,
-                color: widget.groceryItem[index].category.color,
+            itemBuilder: (ctx, index) => Dismissible(
+              onDismissed: (direction) {
+                _removeItem(widget.groceryItem[index]);
+              },
+              key: ValueKey(widget.groceryItem[index].id),
+              child: ListTile(
+                title: Text(widget.groceryItem[index].name),
+                leading: Container(
+                  width: 24,
+                  height: 24,
+                  color: widget.groceryItem[index].category.color,
+                ),
+                trailing: Text(widget.groceryItem[index].quantity.toString()),
               ),
-              trailing: Text(widget.groceryItem[index].quantity.toString()),
             ),
           );
   }
