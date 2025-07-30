55b563a7addfe1c26ae46dc1aa33db8712125b0a
Santi <sweihmuller01@gmail.com>
2025-07-30 00:09:15 -0300

    Create form for adding item

--- lib/Screen/home_screen.dart
+++ lib/Screen/home_screen.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list_app/Widgets/new_item.dart';
 import 'package:shopping_list_app/Widgets/shopping_list_item.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  void _addItem() {
+    Navigator.of(
+      context,
+    ).push(MaterialPageRoute(builder: (context) => const NewItem()));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Your Groceries')),
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+        actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
+      ),
       body: ShoppingListItem(),
     );
   }
